a308915bd2ace00bad970fda6ba1f5741db5d50b
Maxim <graal@inbox.ru>
2024-10-26 21:34:31 +0300

    Директивы и теги

--- Adminka/Adminka/urls.py
+++ Adminka/Adminka/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 
@@ -10,3 +11,6 @@ urlpatterns = [
     path('news/', include('News.urls'))
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Adminka/News/views.py
+++ Adminka/News/views.py
@@ -1,8 +1,5 @@
 from lib2to3.fixes.fix_input import context
-
 from django.shortcuts import render
-# from django.shortcuts import HttpResponse
-
 from .models import News
 
 
@@ -13,9 +10,6 @@ def index(request):
         'title': 'Список новостей'
     }
     return render(request, 'index.html', context=contexts)
-    # res = '<h1>Список новостей</h1>'
-    # for i in news:
-    #     res += f'<div>\n<p>{i.title}</p>\n<p>{i.content}</p>\n</div>\n<hr>\n'
-    # return HttpResponse(res)
+
 
 
