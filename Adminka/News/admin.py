c194b15d0075109762a9d77b7af6835cce2b1543
Maxim <graal@inbox.ru>
2024-10-26 19:04:02 +0300

    Связи моделей и виды шаблона

--- Adminka/News/admin.py
+++ Adminka/News/admin.py
@@ -1,14 +1,19 @@
 from venv import create
 
 from django.contrib import admin
-from .models import News
-
-
+from .models import News, Category
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content', 'created_at', 'photo', 'is_published')
+    list_display = ('id', 'category', 'title', 'content', 'created_at', 'photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_filter = ('is_published', 'id')
+    list_editable = ['is_published', 'category']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
 # Register your models here.
--- Adminka/News/models.py
+++ Adminka/News/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import CharField
 
 class News(models.Model):
     title = models.CharField(max_length=150)
@@ -7,9 +8,19 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to='media/%Y/%m/%d')
     is_published = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     class Meta:
         verbose_name='Новость'
         verbose_name_plural='Новости'
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+
+    class Meta:
+        verbose_name='Категория'
+        verbose_name_plural='Категории'
+        ordering = ['title']
 
