e6ab9ce851b56304086fc2db40de8d1bf87f70fb
Maxim <graal@inbox.ru>
2024-10-26 17:47:25 +0300

    Шаблоны

--- Adminka/Adminka/urls.py
+++ Adminka/Adminka/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/', include('News.urls'))
--- Adminka/News/views.py
+++ Adminka/News/views.py
@@ -1,9 +1,21 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+# from django.shortcuts import HttpResponse
+
+from .models import News
 
 
 def index(request):
-    print(request)
-    return HttpResponse('<h1>Hello Word !')
+    news = News.objects.all()
+    contexts = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, 'index.html', context=contexts)
+    # res = '<h1>Список новостей</h1>'
+    # for i in news:
+    #     res += f'<div>\n<p>{i.title}</p>\n<p>{i.content}</p>\n</div>\n<hr>\n'
+    # return HttpResponse(res)
 
 
