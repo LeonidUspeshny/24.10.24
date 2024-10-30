2bce41994d806c2c8fd4ba5e41a2bf12ca98e595
Maxim <graal@inbox.ru>
2024-10-30 20:15:38 +0300

    Параметры в url-запросах и имена маршрутов

--- Adminka/News/urls.py
+++ Adminka/News/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path
 
 
-from News.views import index
+from News.views import index, get_category
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='Home'),
+    path('category/<int:category_id>/', get_category, name='Category')
 ]
 
--- Adminka/News/views.py
+++ Adminka/News/views.py
@@ -1,15 +1,29 @@
 from lib2to3.fixes.fix_input import context
 from django.shortcuts import render
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     contexts = {
         'news': news,
-        'title': 'Список новостей'
+        'title': 'Список новостей',
+        'categories': categories,
     }
-    return render(request, 'index.html', context=contexts)
+    return render(request, 'News/index.html', context=contexts)
 
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'categories': categories,
+        'category': category
+
+    }
+    return render(request, 'News/category.html', context=context)
 
 
