20dfd9c350ae06e8c73fbd903981f216ad5191e5
Maxim <graal@inbox.ru>
2024-11-11 17:15:04 +0300

    Улучшения и Django Degub Toolbar

--- Adminka/Adminka/urls.py
+++ Adminka/Adminka/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+# from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 
@@ -13,4 +14,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
